51754e789949ce07f144b66acd38b12ec71af56e
Haben Kiflewahd <hkiflewahd@gmail.com>
2025-10-18 18:36:48 +0300

    removed duplicate indexes

--- src/main/java/com/kifiya/promotion_quoter/features/cart/service/CartService.java
+++ src/main/java/com/kifiya/promotion_quoter/features/cart/service/CartService.java
@@ -6,6 +6,5 @@ import com.kifiya.promotion_quoter.features.cart.dto.response.CartQuoteResponse;
 
 public interface CartService {
     CartQuoteResponse quote(CartRequestDto request);
-
     CartConfirmResponse confirm(CartRequestDto request, String idempotencyKey);
 }
